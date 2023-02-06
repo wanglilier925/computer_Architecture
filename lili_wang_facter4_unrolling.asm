851113ce77a9a33c63d7439f78914b4e9cc728f6
wanglilier925 <97651658+wanglilier925@users.noreply.github.com>
2023-02-05 22:28:26 -0500

    Add files via upload
    Project Summary:
    a MIPS processor:
    FP ALU operation FP ALU operation Load float
    Load float
    Load integer Integer ALU operation
    FP ALU operation 4 Store float 3 FP ALU operation 3 Store/Load float 1 Integer ALU operation 1 Integer ALU operation 0
    CSCE 513: Computer Architecture Project 2
     The objective of this project is to create a MIPS assembly code that calculates the Dot Product of two vectors. In linear algebra, the Dot Product is the result of summing the products of the corresponding components in two vectors with equal lengths as expressed in the following:
    𝑛
    𝑎.𝑏= ∑𝑎𝑖𝑏𝑖 𝑖=1
    where 𝑎 and 𝑏 are the first and second vectors, 𝑛 is the length of the vectors, and 𝑎𝑖 and 𝑏𝑖 are the components of vectors 𝑎 and 𝑏, respectively.
    Part A: Assembly Code (60 points)
    Create a MIPS assembly code that calculates the Dot Product of two vectors of length 60 with single precision floating point components.
    ▪ 15 points for the code without unrolling
    ▪ 15 points for the code in which an unrolling with a factor of 2 is utilized
    ▪ 15 points for the code in which an unrolling with a factor of 3 is utilized
    ▪ 15 points for the code in which an unrolling with a factor of 4 is utilized ▪
    IMPORTANT NOTES
    ▪ You are NOT allowed to use Macros.
    ▪ Add comments to each line of your code such that the grader can easily understand your code by reading the comments.
    Part B: Performance Analysis (40 points)
    Given: the below latencies for the dependent Floating-Point (FP) and Integer operations in
          Instruction Producing Results
       Instruction Using Results
       Latency in clock cycles
                       Partial Credit 1: Calculate the IPC of the dot product loop without loop unrolling. (10 pts) Partial Credit 2: What are the IPC values that can be achieved when the dot product loop is unrolled with a factor of 2, 3, and 4? You are allowed to use register-renaming and scheduling methods to speed up the execution. (30 pts)
       Submission
    Please provide a write-up along with your code which includes:
    - A brief description of your code including the samples used to test your code and the results obtained.
    - A description of how IPC is calculated for the loop without unrolling.
    - A description of how IPC is calculated for the three unrolled loops.
    Your write-up should be submitted as a PDF file along with four assembly codes in .asm format for each implementation requested in Part A.

---
+++ lili_wang_facter4_unrolling.asm
@@ -0,0 +1,48 @@
+# d） unrolling with a factor of 4
+.data 
+A: .float 2.0,1.9,3.7,0.6,2.2,0.4,0.2,2.6,2.1,0.9,2.6,2.7,3.7,0.8,0.1,4.2,0.5,2.6,3.1,1.1,0.4,2.6,2.6,2.1,1.7,0.3,2.0,0.7,0.7,2.1,1.6,3.0,0.1,1.8,2.3,2.4,1.0,1.8,5.3,2.7,1.0,2.3,1.6,2.1,1.0,0.5,1.0,2.2,0.7,1.0,0.2,2.7,1.7,2.5,0.7,0.4,1.3,0.3,1.7,0.2
+B: .float 0.5,2.9,1.7,0.3,3.0,1.7,0.1,1.5,0.6,10.9,3.0,1.3,3.0,2.4,0.9,0.4,2.0,1.7,0.5,0.3,0.6,2.8,3.0,3.1,4.2,1.7,2.9,0.5,13.1,1.9,1.3,1.1,0.8,2.7,2.4,1.4,0.3,1.5,2.5,0.8,0.9,1.9,2.8,0.1,2.7,2.8,3.0,1.6,3.0,1.2,1.6,2.6,2.1,2.6,2.0,0.1,1.0,2.3,0.3,2.4
+zero: .float 0
+n:.word 60  # the length of the vectors 
+
+.text 
+la $t0, A   #load  A to $t0
+la $t1, B   #load  B to $t1
+la $t2, n   #load  n to $t2
+lw $s0, 0($t2)  
+
+#inside code
+
+#############
+# initialize accumulator
+	
+l.s   $f7, zero
+	
+loop: 	
+	l.s       $f10, 0($t0)     	 # loading A[i] to $f10
+	l.s       $f12, 0($t1)      	 # loading B[i] to $f12
+	l.s       $f8,  4($t0)     	 # loading A[i-1] to $f8
+	l.s       $f14, 4($t1)      	 # loading B[i-1] to $f14
+	l.s       $f6,  8($t0)     	 # loading A[i-2] to $f6
+	l.s       $f15, 8($t1)      	 # loading B[i-2] to $f15
+	l.s       $f5,  12($t0)     	 # loading A[i-3] to $f5
+	l.s       $f16, 12($t1)      	 # loading B[i-3] to $f16
+	mul.s     $f10, $f10, $f12    	 # calculate a[i]*b[i] -->f10
+	add.s     $f7, $f7, $f10    	 # sum = sum + f10	
+	mul.s     $f9, $f8, $f14    	 # calculate a[i-1]*b[i-1] -->f9	
+	add.s     $f7, $f7, $f9    	 # sum = sum + f9
+	mul.s     $f6, $f6, $f15    	 # calculate a[i-2]*b[i-2] -->f6
+	add.s     $f7, $f7, $f6    	 # sum = sum + f6
+	mul.s     $f5, $f5, $f16    	 # calculate a[i-3]*b[i-3] -->f
+	add.s     $f7, $f7, $f5    	 # sum = sum + f5
+				
+   	addi      $t0, $t0, 16       	 # increment pointer for A[i-4]
+        addi      $t1, $t1, 16        	 # increment pointer for B[i-4]     
+        addi      $s0, $s0, -4		 # i=i-4
+test:
+        bgt       $s0, $zero, loop	 # branch:  continue if i>0
+        
+#############close loop
+li     $v0, 2           # print a float
+mov.s   $f12, $f7         # print the final result in $f12
+syscall          
---
+++ withoutUnrolling.asm
@@ -0,0 +1,36 @@
+# a） without unrolling
+.data 
+
+A: .float 2.0,1.9,3.7,0.6,2.2,0.4,0.2,2.6,2.1,0.9,2.6,2.7,3.7,0.8,0.1,4.2,0.5,2.6,3.1,1.1,0.4,2.6,2.6,2.1,1.7,0.3,2.0,0.7,0.7,2.1,1.6,3.0,0.1,1.8,2.3,2.4,1.0,1.8,5.3,2.7,1.0,2.3,1.6,2.1,1.0,0.5,1.0,2.2,0.7,1.0,0.2,2.7,1.7,2.5,0.7,0.4,1.3,0.3,1.7,0.2
+B: .float 0.5,2.9,1.7,0.3,3.0,1.7,0.1,1.5,0.6,10.9,3.0,1.3,3.0,2.4,0.9,0.4,2.0,1.7,0.5,0.3,0.6,2.8,3.0,3.1,4.2,1.7,2.9,0.5,13.1,1.9,1.3,1.1,0.8,2.7,2.4,1.4,0.3,1.5,2.5,0.8,0.9,1.9,2.8,0.1,2.7,2.8,3.0,1.6,3.0,1.2,1.6,2.6,2.1,2.6,2.0,0.1,1.0,2.3,0.3,2.4
+zero: .float 0
+n:.word 60  # the length of the vectors 
+
+.text 
+la $t0, A   #load  A to $t0
+la $t1, B   #load  B to $t1
+la $t2, n   #load  n to $t2
+lw $s0, 0($t2)  
+
+#inside code
+
+#############
+# initialize accumulator
+	
+l.s   $f7, zero
+	
+loop:
+	l.s       $f10, 0($t0)     	 # loading A[i] to $f10
+	l.s       $f12, 0($t1)      	 # loading B[i] to $f12
+	mul.s     $f10, $f10, $f12    	 # calculate a[i]*b[i] -->f10
+	add.s     $f7, $f7, $f10    	 # sum = sum + f10	
+	addi      $s0, $s0, -1		 # i--
+   	addi      $t0, $t0, 4       	 # increment pointer for A[i], for a float, add 4
+        addi      $t1, $t1, 4        	 # increment pointer for B[i]   
+test:
+        bgtz      $s0, loop	 	#   continue if i>0
+        
+#############close loop
+li     $v0, 2           # print a float
+mov.s   $f12, $f7         # print the final result in $f12
+syscall          
